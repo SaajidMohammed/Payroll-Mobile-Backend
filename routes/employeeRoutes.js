e0f4d0586361f27a32a1360f67472046f24a3e14
Mohammed Saajid K.A <saajidmohammed6@gmail.com>
2026-01-21 19:04:08 +0530

    Updated employee routes

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { addEmployee, getEmployees } = require('../controllers/employeeController');
+// Added getEmployeeById to the destructured imports
+const { addEmployee, getEmployees, getEmployeeById } = require('../controllers/employeeController');
 
-// Standard routes for Employee Management [cite: 48-50]
+// Standard routes for Employee Management
 router.route('/')
-    .get(getEmployees)   // To populate the Employees table [cite: 66]
-    .post(addEmployee);  // For onboarding new employees [cite: 295]
+    .get(getEmployees)   // To populate the Employees table
+    .post(addEmployee);  // For onboarding new employees
+
+// NEW: Route to handle individual employee fetching by ID
+// This fixes the 404 error when navigating to the details screen
+router.route('/:id')
+    .get(getEmployeeById);
 
 module.exports = router;
